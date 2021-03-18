e40807111e8521e1dd265e84d8f165b0d683c9a1
Jayson Barretti <jaysonbarretti@gmail.com>
2021-03-18 19:47:12 +0000

    adicionando linhas no corpo da pagina

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,8 @@ from flask import render_template
 @app.route("/index")
 def index():
     user = { 'username' : 'Jayson'}
-    return render_template("index.html", user = user)
+    posts = [
+        {'author' : {'username' : 'Maria'}, 'body' : "Olá mundo!"},
+        {'author' : {'username' : 'José'}, 'body' : "Olá galera!"},
+    ]
+    return render_template("index.html", user = user, posts = posts)
